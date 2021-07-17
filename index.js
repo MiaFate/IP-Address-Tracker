43e3c7152607ad7a604233932749e2a6f94d2491
MiaFate <miapurplefroot@gmail.com>
2021-07-17 02:56:25 -0300

    eliminado consolelog de prueba

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ async function netlifyEnvs() {
   try {
     const response = await fetch("/.netlify/functions/envsData");
     const data = await response.json();
-    console.log(data);
     return data;
   } catch (error) {
     console.log(error.message);
