f2ad4011500959472991194d649a5c38a45fb92d
MiaFate <miapurplefroot@gmail.com>
2021-07-17 01:16:06 -0300

    test handler

--- netlify/functions/envsData.js
+++ netlify/functions/envsData.js
@@ -1,4 +1,10 @@
-function obtenerEnvsData(){
+/* function obtenerEnvsData(){
 data = process.env.apiKey
 return data
-}
+} */
+exports.handler = (event, context, callback) => {
+    callback(null, {
+      statusCode: 200,
+      body: 'No worries, all is working fine!'
+    })
+  }
